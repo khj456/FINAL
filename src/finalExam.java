9613199a5cb0cd8852cfc736fe08e3bacb4e0c0c
Kim hui jin <khj1382443111@gmail.com>
2024-12-24 00:42:20 +0900

    Update

---
+++ src/finalExam.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class Book {
+	private String title;
+    private String author;
+    private String isbn;
+}
+
+public class finalExam {
+	private ArrayList<Book> bookList = new ArrayList<>();
+	private HashMap<String, ArrayList<String>> borrowRecords = new HashMap<>();
+
+	public finalExam() {
+		
+	}
+	
+	private void bookList() {
+		
+	}
+	
+	private void bookInfo() {
+		
+	}
+	
+	private void rentBook() {
+		
+	}
+	
+	private void returnBook() {
+		
+	}
+	
+	private void addBook() {
+		
+	}
+	
+	public static void main(String[] args) {
+		finalExam program = new finalExam();
+	}
+
+}
